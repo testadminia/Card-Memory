fb06d2f10710d0a59093d2ee148cd186b67b52f0
Kevin Mathew <kevinam99@gmail.com>
2025-04-14 13:13:49 +0000

    Add route to fetch game data

--- backend/routes/memoryRoutes.js
+++ backend/routes/memoryRoutes.js
@@ -7,6 +7,8 @@ const router = express.Router();
 
 // Route to save game data
 router.post("/save", saveGameData);
+
+// Route to fetch game data
 router.get("/fetch", fetchGameData);
 
 module.exports = router;
