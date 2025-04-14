6f0c04d89798b85c049973dfdc605af2ee2125f4
Kevin Mathew <kevinam99@gmail.com>
2025-04-14 13:13:22 +0000

    Add route to fetch game data

--- backend/routes/memoryRoutes.js
+++ backend/routes/memoryRoutes.js
@@ -1,8 +1,12 @@
-const express = require('express');
-const { saveGameData } = require('../controllers/memoryController');
+const express = require("express");
+const {
+  saveGameData,
+  fetchGameData,
+} = require("../controllers/memoryController");
 const router = express.Router();
 
 // Route to save game data
-router.post('/save', saveGameData);
+router.post("/save", saveGameData);
+router.get("/fetch", fetchGameData);
 
 module.exports = router;
